18e30516f277f94c74c3392f3d7e5d415cc34fe4
HerrNerdstromDRK <coding@karrels.com>
2022-03-15 17:47:11 -0500

    added auth 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import './App.css';
+import { Amplify } from 'aws-amplify';
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
     <Authenticator>
       {({ signOut, user }) => (
-        <div className="App">
-          <p>
-            Hey {user.username}, welcome to my channel, with auth!
-          </p>
+        <main>
+          <h1>Hello {user.username}</h1>
           <button onClick={signOut}>Sign out</button>
-        </div>
+        </main>
       )}
     </Authenticator>
   );
 }
-export default App;
